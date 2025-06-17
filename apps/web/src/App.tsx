fa5dfccaa436e26fe61c2f5122e67dc418f67998
Lam <daibanglam@gmail.com>
2025-06-17 14:03:31 +0700

    chore: restructure App component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,16 @@
-import { Toaster } from 'react-hot-toast';
 import { Route, Routes } from 'react-router';
+
 import SignInPage from './auth/pages/SignInPage';
 import MainLayout from './common/templates/MainLayout';
 import DashboardPage from './pages/DashboardPage';
 
 export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/signin" element={<SignInPage />} />
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-        </Route>
-      </Routes>
-      <Toaster position="top-center" />
-    </>
+    <Routes>
+      <Route path="/signin" element={<SignInPage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<DashboardPage />} />
+      </Route>
+    </Routes>
   );
 }
--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -3,10 +3,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import reactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import App from './App';
+
 import './styles.css';
 import { theme } from './theme';
 
+import { Toaster } from 'react-hot-toast';
+import App from './App';
+
+// React Query
 const queryClient = new QueryClient();
 
 const rootEl = document.getElementById('root');
@@ -21,6 +25,7 @@ if (rootEl) {
           {/* React Query */}
           <QueryClientProvider client={queryClient}>
             <App />
+            <Toaster position="top-center" />
           </QueryClientProvider>
         </BrowserRouter>
       </ThemeProvider>
