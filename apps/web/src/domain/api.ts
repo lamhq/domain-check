af3b59c00c97c193cdd2e4a827c87b62284aef41
Lam <daibanglam@gmail.com>
2025-06-18 09:07:45 +0700

    fix: incorrect page size when loading domain list

--- apps/web/src/domain/api.ts
+++ apps/web/src/domain/api.ts
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import type { Domain } from './types';
 
-export async function getDomains(page: number): Promise<[Domain[], number]> {
+export async function getDomains(
+  page: number,
+  size = 5,
+): Promise<[Domain[], number]> {
   try {
+    const offset = (page - 1) * size;
+    const limit = size;
+
     const response = await axios.get<Domain[]>('/api/domains', {
-      params: { page },
+      params: { offset, limit },
     });
-    return [response.data, response.headers['x-total-count']];
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const value = response.headers['x-total-count'];
+    const total = typeof value === 'string' ? parseInt(value, 10) : 0;
+    const pageCount = Math.ceil(total / size);
+    return [response.data, pageCount];
   } catch (error) {
     console.error(error);
     throw new Error('Failed to get domains');
